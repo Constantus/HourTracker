403401146461d8e345f9544b147d50d6dfde1a9f
Constantus <talamantes.matthew@gmail.com>
2020-01-11 23:18:32 -0500

    Added a basic gui to HourTracker to test if the timer works.

--- HourTracker.py
+++ HourTracker.py
@@ -7,10 +7,34 @@ import sys
 import time
 from timer import Timer
 
+
+def startTimer():
+    timer.timerStart()
+    timerRun = True
+
+def pauseTimer():
+    timer.timerPause()
+
+def stopTimer():
+    timer.timerStop()
+
+def updateLabel():
+    timeTuple = timer.getTimeTuple()
+    timeString = '%02d:%02d:%02d:%02d' % (timeTuple[0], timeTuple[1], timeTuple[2], timeTuple[3])
+    label.configure(text=timeString)
+    root.after(10, updateLabel)
 # main code
 
-# Print timer to console for now.
-
-sys.stdout.write("\rCount %d" % count)
-sys.stdout.flush()
-   
+timer = Timer()
+root = tk.Tk()
+timerRun = False
+label = tk.Label(root, text='')
+startButton = tk.Button(root, text='Start', command=startTimer)
+pauseButton = tk.Button(root, text='Pause', command=pauseTimer)
+stopButton = tk.Button(root, text='Stop', command=stopTimer)
+label.pack()
+startButton.pack()
+pauseButton.pack()
+stopButton.pack()
+updateLabel()
+root.mainloop()
