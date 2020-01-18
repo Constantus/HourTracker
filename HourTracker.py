99be015242f229b4d36cac7b4748d478ea0f70f1
Constantus <talamantes.matthew@gmail.com>
2020-01-18 18:30:35 -0500

    Added comments for the functions and made some housecleaning edits.

--- HourTracker.py
+++ HourTracker.py
@@ -9,6 +9,7 @@ from timer import Timer
 
 
 def startPauseTimer():
+    """Starts the timer if it isn't currently running, pauses it if it is."""
     global timerStatus
     if timerStatus == 'paused' or timerStatus == '':
         timer.timerStart()
@@ -21,6 +22,7 @@ def startPauseTimer():
 
 
 def stopTimer():
+    """Stops the timer if it has been started."""
     global timerStatus
     if timerStatus != '':
         timer.timerStop()
@@ -28,18 +30,24 @@ def stopTimer():
             startButton.configure(text='Start')
         timerStatus = 'stopped'
 
-def updateLabel():
-    timeTuple = timer.getTimeTuple()
-    timeString = '%02d:%02d:%02d:%02d' % (timeTuple[0], timeTuple[1], timeTuple[2], timeTuple[3])
-    label.configure(text=timeString)
-    root.after(10, updateLabel)
 
 def resetTimer():
+    """Resets the timer."""
     global timerStatus
     timer.timerReset()
     if timerStatus == 'running' or timerStatus == 'stopped':
         startButton.configure(text='Start')
     timerStatus = ''
+
+
+def updateLabel():
+    """Gets the tuple of the time on the timer and updates the timer label to it."""
+    timeTuple = timer.getTimeTuple()
+    timeString = '%02d:%02d:%02d:%02d' % (timeTuple[0], timeTuple[1], timeTuple[2], timeTuple[3])
+    label.configure(text=timeString)
+    root.after(10, updateLabel)
+
+
 
 # To-Do: Get and do something with the hours returned by timer.timerStop()
 # Add a timerReset button and then also a timer save button and the logic that goes with it.
