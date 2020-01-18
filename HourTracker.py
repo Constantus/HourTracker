b4f1000c6ccc7f40a96ad21eefb9f48cd2ce4ebc
Constantus <talamantes.matthew@gmail.com>
2020-01-18 18:20:32 -0500

    Added reset button and added logic to ensure buttons behave like they are expected to.

--- HourTracker.py
+++ HourTracker.py
@@ -22,14 +22,24 @@ def startPauseTimer():
 
 def stopTimer():
     global timerStatus
-    timer.timerStop()
-    timerStatus = 'stopped'
+    if timerStatus != '':
+        timer.timerStop()
+        if timerStatus == 'running':
+            startButton.configure(text='Start')
+        timerStatus = 'stopped'
 
 def updateLabel():
     timeTuple = timer.getTimeTuple()
     timeString = '%02d:%02d:%02d:%02d' % (timeTuple[0], timeTuple[1], timeTuple[2], timeTuple[3])
     label.configure(text=timeString)
     root.after(10, updateLabel)
+
+def resetTimer():
+    global timerStatus
+    timer.timerReset()
+    if timerStatus == 'running' or timerStatus == 'stopped':
+        startButton.configure(text='Start')
+    timerStatus = ''
 
 # To-Do: Get and do something with the hours returned by timer.timerStop()
 # Add a timerReset button and then also a timer save button and the logic that goes with it.
@@ -41,8 +51,10 @@ root = tk.Tk()
 label = tk.Label(root, text='')
 startButton = tk.Button(root, text='Start', command=startPauseTimer)
 stopButton = tk.Button(root, text='Stop', command=stopTimer)
+resetButton = tk.Button(root, text='Reset', command=resetTimer)
 label.pack()
 startButton.pack()
 stopButton.pack()
+resetButton.pack()
 updateLabel()
 root.mainloop()
