554139356d25829888c10562200752c2549be7f4
Constantus <talamantes.matthew@gmail.com>
2020-01-27 12:28:11 -0500

    When the timer is started, paused or stopped while running. I will return a time string for when it was called.

--- timer.py
+++ timer.py
@@ -10,39 +10,58 @@ class Timer():
         self.instanceTime = 0.0
         self.status = 'stopped'
 
-    
+    def getTimeString(self, timeStamp):
+        '''For internal use, takes a timestamp as an argument and returns the formatted string.
+        In format HH:MM:SSAM/PM'''
+        datetimeObject = dt.fromtimestamp(timeStamp)
+        timeString = datetimeObject.strftime("%I:%M:%S%p")
+        return timeString
+
     def timerStart(self):
-        '''Starts the timer. Creates a startTime class variable that is the timestamp of when the method was called.'''
+        '''Starts the timer. Creates a startTime class variable that is the timestamp of when the method was called.
+        Returns the time it was started.'''
         if self.status != 'running':
             self.startTime = dt.now().timestamp()
             self.status = 'running'
+            startString = self.getTimeString(self.startTime)
+            return startString
         else:
             # Do nothing
             pass
     
 
     def timerPause(self):
-        '''Pauses the timer. But is able to restart when ready, adding to the instanceTime.'''
+        '''Pauses the timer. But is able to restart when ready, adding to the instanceTime.
+        Returns the time it was paused.'''
         if self.status == 'running':
             currentTime = dt.now().timestamp()
             timeDifference = currentTime - self.startTime
             self.instanceTime += timeDifference
             self.status = 'paused'
+            pausedString = self.getTimeString(currentTime)
+            return pausedString
         else:
             # Do nothing
             pass
     
 
     def timerStop(self):
-        '''Stops the timer, returning the final instance time in hours and resetting the instance time to 0.0.'''
+        '''Stops the timer, returning an array of the final instance time in hours and 
+        if the timer was still running the time it stopped. and resetting the instance time to 0.0.'''
+        stopString = ''
         if self.status == 'running':
             currentTime = dt.now().timestamp()
             timeDifference = currentTime - self.startTime
             self.instanceTime += timeDifference
+            stopString = self.getTimeString(currentTime)
         self.status = 'stopped'
         finalTimeSeconds = self.instanceTime
         finalTime = (finalTimeSeconds / 60) / 60
-        return finalTime
+        if stopString != '':
+            returnArray = [finalTime, stopString]
+        else:
+            returnArray = [finalTime]
+        return returnArray
 
     def timerReset(self):
         '''Resets the timer.'''
